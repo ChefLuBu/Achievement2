3932995f7e56b83c2e133ffff14e6b13ef2616e6
Brian Smawley <briansmawley@gmail.com>
2023-05-09 21:58:43 -0400

    Model changes

--- src/src/A2_Recipe_App/recipe_app/models.py
+++ src/src/A2_Recipe_App/recipe_app/models.py
@@ -7,16 +7,49 @@ def validate_minutes(value):
         raise ValidationError('Value must be a positive integer representing minutes.')
     
 
-# Create your models here.
-class Recipe_app(models.Model):
-    name = models.CharField(max_length=200)
-    pic = models.ImageField(upload_to="recipe_name", default='no_picture.jpg')
-    ingredient_name = models.CharField(max_length=200)
-    quantity = models.CharField(max_length=200)
-    unit_of_measurement = models.CharField(max_length=200)
-    directions = models.CharField(max_length=2000)
-    minutes = models.PositiveIntegerField(validators=[validate_minutes])
+# # Create your models here.
+# class Recipe_app(models.Model):
+#     name = models.CharField(max_length=200)
+#     pic = models.ImageField(upload_to="recipe_name", default='no_picture.jpg')
+#     ingredient_name = models.CharField(max_length=200)
+#     quantity = models.CharField(max_length=200)
+#     unit_of_measurement = models.CharField(max_length=200)
+#     directions = models.CharField(max_length=2000)
+#     minutes = models.PositiveIntegerField(validators=[validate_minutes])
 
+
+
+#     def __str__(self):
+#         return self.name
+    
+
+    
+class Recipe_app(models.Model):
+    name = models.CharField(max_length=100)
+    pic = models.ImageField(upload_to='recipe_name', default="no_picture.jpg")
+    ingredients = models.ManyToManyField('Ingredient', through='RecipeIngredient')
+    directions = models.TextField()
+    minutes = models.PositiveIntegerField(validators=[validate_minutes])
 
     def __str__(self):
         return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    unit_of_measurement = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe_app, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField()
+
+    class Meta:
+        unique_together = ('recipe', 'ingredient')
+
+    def __str__(self):
+        return f'{self.recipe} - {self.ingredient}: {self.quantity} {self.ingredient.unit_of_measurement}'
