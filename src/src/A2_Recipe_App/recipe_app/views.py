8c14e3e49a340c306c677a7a5c3579ec58b6dd18
Brian Smawley <briansmawley@gmail.com>
2023-05-11 14:07:06 -0400

    Functioning background for app

--- src/src/A2_Recipe_App/recipe_app/views.py
+++ src/src/A2_Recipe_App/recipe_app/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Recipe_app
 # Create your views here.
 def home(request):
-    return render(request, 'recipe_app/recipes_home.html')
+    recipe=Recipe_app.objects.all()
+    context={
+        'recipe':recipe
+    }
+    return render(request, 'recipe_app/recipes_home.html',context)
 
 def recipe_detail(request, recipe_id):
     recipe=get_object_or_404(Recipe_app, pk=recipe_id)
