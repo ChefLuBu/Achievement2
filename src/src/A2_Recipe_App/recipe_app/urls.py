364f8bff73a47c767cd6c5e2a3ca0a3f627cabfc
Brian Smawley <briansmawley@gmail.com>
2023-05-23 12:27:58 -0400

    Attempting to change landing page
    error in loading landing page

--- src/src/A2_Recipe_App/recipe_app/urls.py
+++ src/src/A2_Recipe_App/recipe_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
-from .views import home, recipe_detail
+from .views import home, recipe_detail, recipe_home
 
 app_name = 'recipe_app'
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
+    path('recipe_home/', recipe_home, name='recipe_home'),
     path('recipe_detail/<int:recipe_id>', recipe_detail, name='recipe_detail')
 ]
--- src/src/A2_Recipe_App/recipe_app/views.py
+++ src/src/A2_Recipe_App/recipe_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Recipe_app
 # Create your views here.
 def home(request):
+        return render(request, 'landing_page/landing_page.html')
+
+def recipe_home(request):
     recipe=Recipe_app.objects.all()
     context={
         'recipe':recipe
