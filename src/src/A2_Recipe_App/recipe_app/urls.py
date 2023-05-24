4aebc32f1f5c8e50288d47633f5922eb925a9026
Brian Smawley <briansmawley@gmail.com>
2023-05-24 12:21:21 -0400

    Reverted several changes
    reverted to commit:
    364f8bff73a47c767cd6c5e2a3ca0a3f627cabfc
    
    added get_absolute_url() to models
    
    in views, replaced recipe_id to pk
    
    in urls, replaced recipe_id to pk for recipe_detail
    
    recipe detail links appear still nonfunctioning

--- src/src/A2_Recipe_App/recipe_app/urls.py
+++ src/src/A2_Recipe_App/recipe_app/urls.py
@@ -4,7 +4,7 @@ from .views import home, recipe_detail, recipe_home
 app_name = 'recipe_app'
 
 urlpatterns = [
-    path('', home, name='home'),
+   path('', home, name='home'),
     path('recipe_home/', recipe_home, name='recipe_home'),
-    path('recipe_detail/<pk>/', recipe_detail, name='recipe_detail'),
+    path('recipe_detail/<pk>', recipe_detail, name='recipe_detail')
     ]
