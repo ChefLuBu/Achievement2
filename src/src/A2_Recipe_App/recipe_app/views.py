8795f4f6af6178e015072ed900b4b7e4abdf965a
Brian Smawley <briansmawley@gmail.com>
2023-06-27 14:36:53 -0400

    Login/Logout Functions
    Addition of login and logout functionality

--- src/src/A2_Recipe_App/recipe_app/views.py
+++ src/src/A2_Recipe_App/recipe_app/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Recipe_app
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView
+
+
 # Create your views here.
+
+# Create your views here.
+
+
 def home(request):
         return render(request, 'recipe_app/landing_page.html')
+
+class RecipeListView(LoginRequiredMixin, ListView):     #class-based “protected” view
+    model = Recipe_app                                  #specify model
+    template_name = 'recipes_home.html'                 #specify template
+
+class RecipeDetailView(LoginRequiredMixin, DetailView):   #class-based “protected” view
+    model = Recipe_app                                   #specify model
+    template_name = 'recipe_detail.html'                 #specify template
+
+@login_required
 
 def recipes_home(request):
     recipe=Recipe_app.objects.all()
